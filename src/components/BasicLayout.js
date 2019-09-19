abeaa6bee7d687b573dc3d8c2557cb9ccfdb070c
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-09-19 20:43:26 +0700

    Complete basic layout (#5)

---
+++ src/components/BasicLayout.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+import { Row, Col } from 'antd';
+import { Navbar } from 'components';
+
+function BasicLayout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <Row>
+        <Col span={20} offset={2} style={{ paddingTop: '50px' }}>
+          {children}
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default BasicLayout;
